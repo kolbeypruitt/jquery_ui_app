a3c4ddb134845fd0b9c4e799d2ec566f725c7737
kolbeypruitt <kolbeypruitt@gmail.com>
2015-10-16 14:07:12 -0600

    iteration four is complete

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
+var db = require('monk')('localhost/jqueryTasks');
+var taskdb = db.get('tasks');
 
 /* GET home page. */
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
+
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  taskdb.find({}, function (err, tasks) {
+    taskdb.distinct("category", function (err, categories) {
+      res.render('index', {allCategories: categories, tasks: tasks});
+    })
+  })
 });
+
 
 module.exports = router;
